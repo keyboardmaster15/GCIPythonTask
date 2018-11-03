5ec1a3939366fcecd242c7411a1e91f7724eaca6
keyboardmaster15 <34858487+keyboardmaster15@users.noreply.github.com>
2018-11-03 15:46:40 +0200

    Add files via upload

---
+++ gci.py
@@ -0,0 +1,17 @@
+for i in range(10):
+    print( "GCI is great")
+	
+	
+	
+	
+x = input("what is your name?")
+
+
+print("Hello "+(x)+", please to meet you!")
+
+
+
+print("you're name backwards is " + x[::-1])
+
+
+
